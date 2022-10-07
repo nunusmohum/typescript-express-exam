8e07298bf9e9b5ee51c59b5cccbcfa38a67b4256
nunusmohum <halojw301@gmail.com>
2022-10-07 16:49:46 +0900

    setup

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ const port: number = 8000;
 
 app.get("/testsdassd", (req: express.Request, res: express.Response) => {
   console.log(req);
-  res.send({ name: "yoon sang seok", age: 99, friends: ["ss", "ys", "ye"] });
+  res.send({ name: "LEE JU WON", age: 99, friends: ["ss", "ys", "ye"] });
   res.send("sadjklas");
 });
 
